bf302efdf54c22bd287696621c7f236dc546a41d
splancher <splancher@unibz.it>
2024-04-15 01:01:46 +0200

    Fixed chatbot widget rendering twice

--- Frontend/src/Components/dashboard/chatbot/chatbot.js
+++ Frontend/src/Components/dashboard/chatbot/chatbot.js
@@ -1,49 +1,52 @@
-import React, { useEffect } from 'react'
+import React, {useEffect, useRef} from 'react'
 import "./chatbot.css"
 
 const Chatbot = () => {
+    const initialized = useRef(false);
     useEffect(() => {
-        const script = document.createElement('script')
-        script.src = 'https://cdn.botpress.cloud/webchat/v1/inject.js'
-        script.async = true
-        document.body.appendChild(script)
+        if(!initialized.current){
+            initialized.current = true;
+            const script = document.createElement('script')
+            script.src = 'https://cdn.botpress.cloud/webchat/v1/inject.js'
+            script.async = true
+            document.body.appendChild(script)
 
-        script.onload = () => {
-            window.botpressWebChat.init({
-                botId: 'a8c05c56-6565-4601-966f-680ae905670b',
-                hostUrl: 'https://cdn.botpress.cloud/webchat/v1',
-                messagingUrl: 'https://messaging.botpress.cloud',
-                clientId: 'a8c05c56-6565-4601-966f-680ae905670b',
-                webhookId: '08abd269-979b-4f38-9588-3919a4c7b25f',
-                avatarUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/UniBZ-Logo.svg/1200px-UniBZ-Logo.svg.png',
-                showBotInfoPage: true,
-                lazySocket: true,
-                themeName: 'prism',
-                theme: 'prism',
-                themeColor: '#2563eb',
-                frontendVersion: 'v1',
-                botName: 'unibzBot',
-                enableConversationDelete: true,
-                botConversationDescription: "Thesis Chatbot by Simon Plancher",
-                useSessionStorage: true,
-                composerPlaceholder: "Chat with unibzBot"
-            });
+            script.onload = () => {
+                window.botpressWebChat.init({
+                    botId: 'a8c05c56-6565-4601-966f-680ae905670b',
+                    hostUrl: 'https://cdn.botpress.cloud/webchat/v1',
+                    messagingUrl: 'https://messaging.botpress.cloud',
+                    clientId: 'a8c05c56-6565-4601-966f-680ae905670b',
+                    webhookId: '08abd269-979b-4f38-9588-3919a4c7b25f',
+                    avatarUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/UniBZ-Logo.svg/1200px-UniBZ-Logo.svg.png',
+                    showBotInfoPage: true,
+                    lazySocket: true,
+                    themeName: 'prism',
+                    theme: 'prism',
+                    themeColor: '#2563eb',
+                    frontendVersion: 'v1',
+                    botName: 'unibzBot',
+                    enableConversationDelete: true,
+                    botConversationDescription: "Thesis Chatbot by Simon Plancher",
+                    useSessionStorage: true,
+                    composerPlaceholder: "Chat with unibzBot"
+                });
 
-            window.botpressWebChat.onEvent(
-                function () {
-                    window.botpressWebChat.sendEvent({ type: "show" });
-                },
-                ["LIFECYCLE.LOADED"]
-            )
-            window.botpressWebChat.onEvent(
-                (event) => {
-                    if(event.type === 'MESSAGE.RECEIVED') {
-                        console.log(event.value.payload.text);
-                    }
-                },
-                ['MESSAGE.RECEIVED']
-            )
-
+                window.botpressWebChat.onEvent(
+                    function () {
+                        window.botpressWebChat.sendEvent({ type: "show" });
+                    },
+                    ["LIFECYCLE.LOADED"]
+                )
+                window.botpressWebChat.onEvent(
+                    (event) => {
+                        if(event.type === 'MESSAGE.RECEIVED') {
+                            console.log(event.value.payload.text);
+                        }
+                    },
+                    ['MESSAGE.RECEIVED']
+                )
+            }
         }
     }, [])
 
--- Frontend/src/Components/dashboard/dashboard.js
+++ Frontend/src/Components/dashboard/dashboard.js
@@ -1,30 +1,23 @@
 import React from 'react';
 import Chatbot from "./chatbot/chatbot";
 import './dashboard.css';
-import { ReactComponent as ArrowSvg } from './arrow-curved.svg';
 import {Col, Container, Row} from "react-bootstrap";
 
 const Dashboard = () => {
     return (
         <div className="Dashboard">
-            <Container className="d-flex flex-column justify-content-center align-items-center min-vh-100">
+            <Container className="d-flex flex-column justify-content-center align-items-center">
                 <Row className="mb-3">
                     <Col>
                         <h1>Try out unibzBot here</h1>
                         <p>
                             unibzBot will try to answer every University related question.
+                            <br/>
                             It is not able to answer questions that are not UNIBZ related.
                         </p>
                     </Col>
                 </Row>
-                <Row className="align-items-center">
-                    <Col>
-                        <Chatbot />
-                    </Col>
-                    <Col xs="auto">
-                        <ArrowSvg className="arrow" />
-                    </Col>
-                </Row>
+                <Chatbot/>
             </Container>
         </div>
     );
