f2b5bb6244997529a98297e06e725d95095eab83
splancher <splancher@unibz.it>
2024-04-22 15:45:29 +0200

    Added instruction picture

--- src/Components/dashboard/dashboard.js
+++ src/Components/dashboard/dashboard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Chatbot from "./chatbot/chatbot";
 import './dashboard.css';
+import WidgetPointer from '../../Images/widget.png';
 import {Col, Container, Row} from "react-bootstrap";
 
 const Dashboard = () => {
@@ -14,7 +15,16 @@ const Dashboard = () => {
                             unibzBot will try to answer every university related question.
                             <br/>
                             It is not able to answer questions that are not UNIBZ related.
+                            <br/>
+                            Click on the icon in the bottom right to try out unibzBot!
+
                         </p>
+                    </Col>
+                </Row>
+                <Row className="mb-3">
+                    <Col className={"d-flex justify-content-center"}>
+
+                        <img src={WidgetPointer} alt={"Widget Pointer"}/>
                     </Col>
                 </Row>
                 <Chatbot/>
